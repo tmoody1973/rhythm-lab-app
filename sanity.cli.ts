5ec1187d8b3f2bb0b4dec8d14c8ae7db93397b2a
Tarik Moody <tarikjmoody@gmail.com>
2026-05-17 16:17:41 -0500

    chore: add sanity.cli.ts with studio deployment config

---
+++ sanity.cli.ts
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'b9cutvrc',
+    dataset: 'production',
+  },
+  studioHost: 'rhythmlab',
+  deployment: {
+    appId: 'cvruhsq0bs9rwvew94h0xr70',
+  },
+})
