81c335b684b809befdb5cdbc7b9151f3f428baa8
Tarik Moody <tarikjmoody@gmail.com>
2026-05-17 12:03:11 -0500

    feat: add GROQ queries for blog posts

---
+++ lib/sanity/queries/posts.ts
@@ -0,0 +1,37 @@
+import { defineQuery } from 'next-sanity'
+
+export const ALL_POSTS_QUERY = defineQuery(`
+  *[_type == "post"] | order(publishedAt desc) {
+    _id,
+    title,
+    subtitle,
+    "slug": slug.current,
+    publishedAt,
+    excerpt,
+    coverImage,
+    readingTime,
+    "tags": tags[]->{label, "slug": slug.current},
+    "author": author->{name, avatar}
+  }
+`)
+
+export const POST_BY_SLUG_QUERY = defineQuery(`
+  *[_type == "post" && slug.current == $slug][0] {
+    _id,
+    title,
+    subtitle,
+    "slug": slug.current,
+    publishedAt,
+    excerpt,
+    coverImage,
+    body,
+    readingTime,
+    "tags": tags[]->{label, "slug": slug.current},
+    "author": author->{name, avatar},
+    seo
+  }
+`)
+
+export const ALL_POST_SLUGS_QUERY = defineQuery(`
+  *[_type == "post"]{"slug": slug.current}
+`)
